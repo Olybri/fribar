3725b1cfbd29890801c46af6b59e569192177ef8
Loris Witschard <loris.wit@gmail.com>
2017-03-30 22:19:32 +0200

    Ajoute un pied de page

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once "include/database.php";
 require_once "include/template.php";
+require_once "include/revision.php";
 
 require "fribar/".(filter_input(INPUT_GET, "url") ?: "home").".php";
 
@@ -11,5 +12,10 @@ if(!isset($title) || !isset($tpl))
 $main_tpl = new Template("main");
 $main_tpl->set("title", $title);
 $main_tpl->set("content", $tpl->html());
+
+$footer = $git_not_found ? "" :
+    "FriBar revision #$rev_num ($last_rev)<br>$rev_date";
+
+$main_tpl->set("footer", $footer);
 
 echo $main_tpl->html();
