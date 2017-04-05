2b22ad0e0a1f05c52c5cb3b458196d6e8c257ae8
Loris Witschard <loris.wit@gmail.com>
2017-04-05 19:25:06 +0200

    Ajoute un formulaire pour suggérer une bière

--- fribar/beer.php
+++ fribar/beer.php
@@ -1,6 +1,7 @@
 <?php
 
-if(!isset($_GET["id"]))
+// Si on veut la liste des bières
+if(!isset($_GET["id"]) && empty($_POST))
 {
     $title = "Bières";
     $tpl = new Template("beer_list");
@@ -16,17 +17,16 @@ if(!isset($_GET["id"]))
     $tpl->set("beer_table", $beer_table);
 }
 
-else
+// Si on veut consulter une bière
+else if(isset($_GET["id"]))
 {
-    $res = $db->query("SELECT name FROM product WHERE id = ".$_GET["id"]);
-    if($res->rowCount() == 0)
+    $title = $db->query("SELECT name FROM product WHERE id = ".$_GET["id"])->fetchColumn();
+    if(!$title)
         die("Produit inexistant.");
     
-    $title = $res->fetch()["name"];
+    $service_list = $db->query("SELECT * FROM service WHERE product_id = ".$_GET["id"]);
     
-    $res = $db->query("SELECT * FROM service WHERE product_id = ".$_GET["id"]);
-    
-    if($res->rowCount() == 0)
+    if($service_list->rowCount() == 0)
         $tpl = new Template("not_served");
     
     else
@@ -34,13 +34,13 @@ else
         $tpl = new Template("beer");
         
         $bar_table = "";
-        foreach($res as $row)
+        foreach($service_list as $service)
         {
-            $bar = $db->query("SELECT * FROM bar WHERE id = ".$row["bar_id"])->fetch();
+            $bar = $db->query("SELECT * FROM bar WHERE id = ".$service["bar_id"])->fetch();
             $bar_table .= "<tr>"
                 ."<td><a href='bar?id=".$bar["id"]."'>".$bar["name"]."</td>"
-                ."<td>".($row["volume"] / 100)." dl</td>"
-                ."<td>CHF ".($row["price"] / 100)."</td>"
+                ."<td>".($service["volume"] / 100)." dl</td>"
+                ."<td>CHF ".($service["price"] / 100)."</td>"
                 ."</tr>\n";
         }
         
@@ -48,4 +48,21 @@ else
     }
 }
 
-?>
+// Si on veut ajouter une bière
+else if(isset($_POST["form"]))
+{
+    $title = "Ajouter une bière";
+    $tpl = new Template("beer_form");
+}
+
+// Si on veut soumettre une bière
+else if(isset($_POST["submit"]))
+{
+    $title = "Bières";
+    $tpl = new Template("data_sent");
+    
+    $db->query("INSERT INTO product (name) VALUES ('".$_POST["name"]."')");
+    
+    $tpl->set("text", "Nouvelle bière envoyée");
+    $tpl->set("back", "/beer");
+}
